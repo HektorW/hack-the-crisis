3c3049cc9493cf431cbdabb72a3a8ed4c98ef70a
Joel Gullander <jag@joelgullander.se>
2020-04-05 14:27:33 +0200

    Show recent answers by enum

--- client/src/components/_conversation-form/ConversationFormQuestion/ConversationFormQuestion.tsx
+++ client/src/components/_conversation-form/ConversationFormQuestion/ConversationFormQuestion.tsx
@@ -4,9 +4,11 @@ import React from 'react'
 import { useSyncFormQuestionValue } from './ConversationFormQuestion.hooks'
 
 import './conversation-form-question.scss'
+import { useGetSelfReports } from '../../../hooks/selfReportsHooks'
+import HealthCheckNames from '../../../enums/HealthCheckNames'
 
 export interface ConversationFormQuestionProps {
-  name: string
+  name: HealthCheckNames
   value: any
 
   heading: string
@@ -26,11 +28,21 @@ export default function ConversationFormQuestion({
 }: ConversationFormQuestionProps) {
   useSyncFormQuestionValue(name, value)
 
+  console.log(name)
+  const reports = useGetSelfReports(name);
+  console.log(reports)
   return (
     <div className={classNames('conversation-form-question', className)}>
       <h3 className="conversation-form-question__heading">{heading}</h3>
 
       <div className="conversation-form-question__content">{children}</div>
+
+      {reports && reports.length > 0 &&
+        <div className="conversation-form-question__recent">
+          Last report: <span>{reports[reports.length - 1].answeredDate.split('T')[0]}</span> <br />
+          <h2>{reports[reports.length - 1].answerId }</h2>
+        </div>
+      }
     </div>
   )
 }
--- client/src/hooks/selfReportsHooks.ts
+++ client/src/hooks/selfReportsHooks.ts
@@ -5,6 +5,7 @@ import firebase from 'firebase';
 import { useCallback } from 'react';
 import { useGetUser } from './useGetUser';
 import HealthCheckNames from '../enums/HealthCheckNames';
+import { BreathingProblemAnsers, CoronaContactAnswers, CoughAnswers, EnergyAnswers } from '../enums/HealthCheckAnswers';
 
 export function useGetSelfReports(type: HealthCheckNames) {
   const user = useGetUser();
@@ -19,10 +20,36 @@ export function useGetSelfReports(type: HealthCheckNames) {
 
   const selfReport = useSelector((state: AppState) => state.firestore.ordered.selfreports);
 
-  // TODO, make more beautiful
+  // TODO, make more beautiful & Transform the enum values to the same dialog texts.
   if(selfReport && selfReport.length > 0) {
-    return selfReport[0].questionaries.filter(x => x.questionId === type)
+    return selfReport[0].questionaries.filter(x => x.questionId === type).map(question => {
+      let mappedAnswer : string | number;
+      switch(question.questionId) {
+        case HealthCheckNames.BreathingProblem:
+          mappedAnswer = BreathingProblemAnsers[question.answerId]
+          break;
+        case HealthCheckNames.CoronaContact:
+          mappedAnswer = CoronaContactAnswers[question.answerId]
+          break;
+        case HealthCheckNames.Coughing:
+          mappedAnswer = CoughAnswers[question.answerId]
+          break;
+        case HealthCheckNames.Energy:
+          mappedAnswer = EnergyAnswers[question.answerId]
+          break;
+        default:
+          mappedAnswer = question.answerId
+          break;
+      }
+
+      return {
+        ...question,
+        answerId: mappedAnswer
+      }
+    })
   }
+
+
 
   return null
 }
